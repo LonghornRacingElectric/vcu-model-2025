058b7d01799bdba03756a156b62e64e3ad9ea472
Dhairya Gupta <dhairyag23@gmail.com>
2024-11-18 06:41:08 -0600

    updtes

--- sil/main.cpp
+++ sil/main.cpp
@@ -3,9 +3,15 @@
 //
 
 #include <pybind11/pybind11.h>
+#include <stdio.h>
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(vcu, module_handle) {
+void eval() { printf("evaluating in the main loop rn lol\n"); }
 
+PYBIND11_MODULE(vcu, module_handle) {
+    module_handle.doc() =
+        "Evaluation method, defined in the vcu-model library. Check source for "
+        "parameters.";
+    module_handle.def("eval", &eval);
 }
