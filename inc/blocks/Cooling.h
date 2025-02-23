7296e5789d8786d47996d45c72855548c0a6e6bd
Alex Huang <alexandreahuang@gmail.com>
2025-02-23 17:09:13 -0600

    cooling circuit

--- inc/blocks/Cooling.h
+++ inc/blocks/Cooling.h
@@ -9,26 +9,36 @@
 
 #include "../VcuParameters.h"
 
+#define MOTOR_FAN_ON_THRESHOLD 40.0f
+#define MOTOR_FAN_FULL_SPEED_THRESHOLD 60.0f
+#define MOTOR_TEMP_DIFF_THRESHOLD 5.0f
+
+#define BATT_FAN_ON_THRESHOLD 30.0f
+#define BATT_FAN_FULL_SPEED_THRESHOLD 35.0f //shouldn't be higher than 60.0
+#define BATT_TEMP_DIFF_THRESHOLD = 2.5f
 
 typedef struct CoolingInput {
-    float batteryTemp;
-    float inverterTemp;
-    float motorTemp;
+    float battRadInTemp; //different sensors
+    float battRadOutTemp;
+    float motorRadInTemp;
+    float motorRadOutTemp;
+    float motorInverterTemp;
+    float ambientTemp;
+    int battTach; // assumed in RPM
+    int motorTach;
 } CoolingInput;
 
 
 typedef struct CoolingOutput {
-    float pumpOutput;
-    float radiatorOutput;
+    float pump1Output;
+    float pump2Output;
+    float motorFansOutput;
     float batteryFansOutput;
 } CoolingOutput;
 
-void Cooling_evaluate(VcuParameters *params, CoolingInput *input, CoolingOutput *output, float deltaTime);
 
 
-// class Cooling {
-// public:
-//     void setParameters(VcuParameters* params);
-//     void evaluate(VcuParameters *params, CoolingInput *input, CoolingOutput *output, float deltaTime);
-// };
+void Cooling_evaluate(VcuParameters *params, CoolingInput *input, CoolingOutput *output, int desiredRPM);
+
+
 #endif //COOLING_H
--- src/blocks/Cooling.c
+++ src/blocks/Cooling.c
@@ -3,13 +3,82 @@
 //
 
 #include "../../inc/blocks/Cooling.h"
+#include "util/Timer.h"
 
 
-void Cooling_evaluate(VcuParameters *params, CoolingInput *input, CoolingOutput *output, float deltaTime) {
-    // this code outputs max cooling all the time
-    output->pumpOutput = 1.0f;
-    Lookup1D_evaluate(&params->coolingMotor, input->motorTemp);
-    Lookup1D_evaluate(&params->coolingMotor, input->batteryTemp);
-    // output->radiatorOutput = params->coolingMotor(input->motorTemp);
-    // output->batteryFansOutput = params->coolingMotor(input->batteryTemp);
+void cooling_motor(VcuParameters *params, CoolingInput *input, CoolingOutput *output){
+    output->pump1Output = 1.0f; //pumps for motor should be on all the time
+    Lookup1D radLookup;
+    Lookup1D_init(&radLookup, 1.0, 24.0);
+
+    //temp thresholds
+
+
+    float fan_speed = 0.0f;
+
+
+    if (input->motorRadInTemp >= MOTOR_FAN_ON_THRESHOLD || input->motorRadOutTemp >= MOTOR_FAN_ON_THRESHOLD || input->motorInverterTemp >= MOTOR_FAN_ON_THRESHOLD){
+        if (input->motorRadInTemp >= MOTOR_FAN_FULL_SPEED_THRESHOLD || input->motorRadOutTemp >= MOTOR_FAN_FULL_SPEED_THRESHOLD || input->motorInverterTemp >= MOTOR_FAN_FULL_SPEED_THRESHOLD){
+            fan_speed = 1.0f;
+        }
+        else{
+            fan_speed = (input->motorInverterTemp - MOTOR_FAN_ON_THRESHOLD)/MOTOR_FAN_FULL_SPEED_THRESHOLD;
+        }
+    }
+    output->motorFansOutput = fan_speed;
+
+    float delta_temp = input->motorRadInTemp - input->motorRadOutTemp;
+
+    //this is #superscuffed
+    Timer check;
+    Timer_init(&check, 5); // wait 5 seconds
+//    for (int i = 0; i < 5; i++) {
+        Timer_count(&check, 1);
+//    }
+    if (Timer_isFinished(&check)){ //timer is done
+        if (delta_temp < MOTOR_TEMP_DIFF_THRESHOLD) {
+            fan_speed = 1.0f; // If radiator isn't cooling well, max fan speed
+        }
+    }
+    output->motorFansOutput = fan_speed;
+}
+
+void cooling_bat(VcuParameters *params, CoolingInput *input, CoolingOutput *output){
+    if (input->battRadOutTemp >= input->ambientTemp){ //wait for battery temp to reach ambient temp
+        output->pump2Output = 1.0f;
+    }
+    //temp thresholds
+
+
+    //fan speed
+    float fan_speed = 0.0f;
+
+    //look up table
+    Lookup1D coolingLookup;
+    Lookup1D_init(&coolingLookup, 1.0, 24.0);
+
+
+    if (input->battRadInTemp >= BATT_FAN_ON_THRESHOLD || input->battRadOutTemp >= BATT_FAN_ON_THRESHOLD){
+        if (input->battRadInTemp >= BATT_FAN_FULL_SPEED_THRESHOLD || input->battRadOutTemp >= BATT_FAN_FULL_SPEED_THRESHOLD){
+            fan_speed = 1.0f;
+        }
+        else{
+            fan_speed = (input->battRadOutTemp - BATT_FAN_ON_THRESHOLD)/BATT_FAN_FULL_SPEED_THRESHOLD;
+        }
+    }
+    output->batteryFansOutput = fan_speed;
+
+    //this is still #superscuffed
+    float delta_temp = input->battRadOutTemp - input->battRadInTemp;
+    Timer check;
+    Timer_init(&check, 5); // wait 5 seconds
+    for (int i = 0; i < 5; i++) {
+        Timer_count(&check, 1);
+    }
+    if (Timer_isFinished(&check)){ //timer is done
+        if (delta_temp < TBATT)EMP_DIFF_THRESHOLD) {
+            fan_speed = 1.0f; // if hasn't cooled enough, max fan speed
+        }
+    }
+    output->motorFansOutput = fan_speed;
 }
